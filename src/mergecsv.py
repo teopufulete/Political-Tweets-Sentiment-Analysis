3832852d90acefb35afdf0ddb76e708534f468ee
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-03-27 15:27:46 +0200

    merge all csv files into 1

---
+++ src/mergecsv.py
@@ -0,0 +1,10 @@
+import os
+import glob
+import pandas as pd
+os.chdir("D:\cs\projects\dissertation\data\concatenate")
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
